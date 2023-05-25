db19df3d512985c330a94e40860d6b8da3b43562
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-25 19:19:13 +0300

    Completed Assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Pets (
+  id INTEGER,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Playful', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 3, 'M', 'Black', 'Energetic', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 4, 'F', 'Gray', 'Calm', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 6, 'M', 'Brown', 'Brave', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 2, 'F', 'White', 'Friendly', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 7, 'M', 'Green', 'Mischievous', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 4, 'F', 'Brown', 'Sweet', 1);
+INSERT INTO Pets (id, name, age, sex, color, temperament, alive) VALUES (8, 'unnamed', NULL, 'F', 'Black', NULL, 1);
